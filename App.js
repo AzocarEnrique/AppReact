8277b45b6889a0663a747d30fe278c0794db2f03
Cesar Sobarzo <cesarsobarzo.huenchuan@gmail.com>
2023-04-24 21:37:49 -0400

    Creacion de bases

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola</Text>
+      <Text>Hola assa s</Text>
       <StatusBar style="auto" />
     </View>
   );
